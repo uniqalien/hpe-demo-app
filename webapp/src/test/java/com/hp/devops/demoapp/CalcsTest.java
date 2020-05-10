2b9a4348c73893e1ca5989ed2369c6ed3fcef39d
Uniq Alien <12989986+uniqalien@users.noreply.github.com>
2020-05-10 23:28:16 +0300

    yo
    commit

--- webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
@@ -8,10 +8,6 @@ import static org.junit.Assert.assertTrue;
 /**
  * Created by  gullery                  on 17/02/2016. testtted in pipeline 111
 <<<<<<< HEAD
- * this is new *asdwej
-=======
- * this is new
->>>>>>> c00a1ac6278f44ae8e7dbdffc6a6538a717dcf09
  */
 public class CalcsTest {
 	@Test
