7b5f018e00e36a84d00c5f38c1b666bad47a7295
Uniq Alien <12989986+uniqalien@users.noreply.github.com>
2020-05-11 12:11:38 +0300

    upd

--- webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/CalcsTest.java
@@ -6,6 +6,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class CalcsTest {
+	///
+	///
+	///
 	@Test
 	public void sum_test_A() {
 		Calcs.someTouchedMethod();
