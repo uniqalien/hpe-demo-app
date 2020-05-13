bf9516c2eb91e2024698a0651a10b9073cb627f4
vladislav-papava <vladislav.papava@microfocus.com>
2020-05-13 15:40:04 +0300

    feature #1392 asdasdasdsad

--- webapp/src/main/java/test/Utils.java
+++ webapp/src/main/java/test/Utils.java
@@ -1,16 +1,5 @@
 package test;
 
-/**
- *  Created by vaingato              on 05/12/2016.
- *  Testing hotspot  widget by Ralu
- */
- */
- */
- */
- */
- */
- */
-
 import java.util.ArrayList;
 
 public class Utils {
